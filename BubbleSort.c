ae96227337b91bc92010cc7547234527867020c7
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2018-07-25 07:08:35 +0530

    BubbleSort

---
+++ BubbleSort.c
@@ -0,0 +1,49 @@
+/*
+----- Bubble Sort Algorithm -----
+
+Author: https://github.com/ravikumark815
+*/
+#include<stdio.h>
+
+int BubbleSort(int a[],int n);
+
+int main()
+{
+    int n=0,i=0,a[50];
+
+    printf("\n------- Bubble Sort -------\n");
+    printf("Enter the number of elements (Max:50):\t");
+    scanf("%d",&n);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Enter Element#%d:\t",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("\nUnsorted Array:\n");
+    for(i=0;i<n;i++)
+        printf("| %d ",a[i]);
+    printf("|\n");
+
+    BubbleSort(a, n);
+
+    printf("\nSorted Array:\n");
+    for(i=0;i<n;i++)
+        printf("| %d ",a[i]);
+    printf("|\n");
+
+    return 0;
+}
+int BubbleSort(int a[],int n)
+{
+    int i=0,j=0;
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n-i-1;j++)
+            if(a[j]>a[j+1])
+            {
+                a[j] = a[j]+a[j+1];
+                a[j+1] = a[j]-a[j+1];
+                a[j] = a[j]-a[j+1];
+            }
+}
