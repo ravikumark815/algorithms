ef1cda7c97b046789e9810bae870199cdb435abf
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2018-08-23 18:06:57 +0530

    QuickSort

---
+++ QuickSort.c
@@ -0,0 +1,65 @@
+/*
+----- Quick Sort Algorithm -----
+
+Author: https://github.com/ravikumark815
+*/
+#include<stdio.h>
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int Partition(int a[], int low, int high)
+{
+    int i=low-1;
+    int pivot = a[high];
+    for(int j=low;j<=high-1;j++)
+    {
+        if(a[j]<=pivot)
+        {
+            i++;
+            swap(&a[i],&a[j]);
+        }
+    }
+    swap(&a[i+1],&a[high]);
+
+    return i+1;
+}
+int QuickSort(int a[],int low, int high)
+{
+    if(low<high)
+    {   
+        int pivot = Partition(a, low, high);
+        QuickSort(a, low, pivot-1);
+        QuickSort(a, pivot+1, high);
+    }
+}
+
+int main()
+{
+    int n=0,i=0,a[50];
+
+    printf("\n------- Quick Sort -------\n");
+    printf("Enter the number of elements (Max:50):\t");
+    scanf("%d",&n);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Enter Element#%d:\t",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("\nUnsorted Array:\n");
+    for(i=0;i<n;i++)
+        printf("| %d ",a[i]);
+    printf("|\n");
+
+    QuickSort(a,0,n-1);
+
+    printf("\nSorted Array:\n");
+    for(i=0;i<n;i++)
+        printf("| %d ",a[i]);
+    printf("|\n");
+
+    return 0;
+}
